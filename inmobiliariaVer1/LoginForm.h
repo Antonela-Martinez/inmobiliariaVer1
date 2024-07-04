95ff9c143340c00abc3debd70c72bcfc42025333
Antonela Martinez <martinezantonela36@gmail.com>
2024-07-04 19:00:31 -0300

    Create new breach Rama1 and Create loging form

---
+++ inmobiliariaVer1/LoginForm.h
@@ -0,0 +1,225 @@
+#pragma once
+
+namespace inmobiliariaVer1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System::Data::SqlClient;
+
+	/// <summary>
+	/// Resumen de LoginForm
+	/// </summary>
+	public ref class LoginForm : public System::Windows::Forms::Form
+	{
+	public:
+		LoginForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~LoginForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ tbEmail;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ tbPassword;
+	private: System::Windows::Forms::Button^ btnOK;
+	private: System::Windows::Forms::Button^ btnCancelar;
+
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->tbEmail = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->tbPassword = (gcnew System::Windows::Forms::TextBox());
+			this->btnOK = (gcnew System::Windows::Forms::Button());
+			this->btnCancelar = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 26, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->label1->Location = System::Drawing::Point(4, 3);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(987, 100);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Loging";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"MS Reference Sans Serif", 16, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::SystemColors::ActiveCaption;
+			this->label2->Location = System::Drawing::Point(80, 168);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(126, 40);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Email:";
+			this->label2->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// tbEmail
+			// 
+			this->tbEmail->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tbEmail->Location = System::Drawing::Point(309, 165);
+			this->tbEmail->Margin = System::Windows::Forms::Padding(4, 3, 4, 3);
+			this->tbEmail->Name = L"tbEmail";
+			this->tbEmail->Size = System::Drawing::Size(651, 44);
+			this->tbEmail->TabIndex = 2;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"MS Reference Sans Serif", 16, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::SystemColors::ActiveCaption;
+			this->label3->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->label3->Location = System::Drawing::Point(64, 269);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(192, 40);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"Password:";
+			// 
+			// tbPassword
+			// 
+			this->tbPassword->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tbPassword->Location = System::Drawing::Point(309, 269);
+			this->tbPassword->Margin = System::Windows::Forms::Padding(4, 3, 4, 3);
+			this->tbPassword->Name = L"tbPassword";
+			this->tbPassword->PasswordChar = '*';
+			this->tbPassword->Size = System::Drawing::Size(651, 44);
+			this->tbPassword->TabIndex = 4;
+			// 
+			// btnOK
+			// 
+			this->btnOK->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnOK->ForeColor = System::Drawing::Color::MediumAquamarine;
+			this->btnOK->Location = System::Drawing::Point(309, 361);
+			this->btnOK->Name = L"btnOK";
+			this->btnOK->Size = System::Drawing::Size(292, 55);
+			this->btnOK->TabIndex = 5;
+			this->btnOK->Text = L"OK";
+			this->btnOK->UseVisualStyleBackColor = true;
+			this->btnOK->Click += gcnew System::EventHandler(this, &LoginForm::btnOK_Click);
+			// 
+			// btnCancelar
+			// 
+			this->btnCancelar->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnCancelar->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)));
+			this->btnCancelar->Location = System::Drawing::Point(675, 361);
+			this->btnCancelar->Name = L"btnCancelar";
+			this->btnCancelar->Size = System::Drawing::Size(285, 55);
+			this->btnCancelar->TabIndex = 6;
+			this->btnCancelar->Text = L"Cancelar";
+			this->btnCancelar->UseVisualStyleBackColor = true;
+			this->btnCancelar->Click += gcnew System::EventHandler(this, &LoginForm::btnCancelar_Click);
+			// 
+			// LoginForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::GradientActiveCaption;
+			this->ClientSize = System::Drawing::Size(991, 450);
+			this->Controls->Add(this->btnCancelar);
+			this->Controls->Add(this->btnOK);
+			this->Controls->Add(this->tbPassword);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->tbEmail);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Margin = System::Windows::Forms::Padding(4, 3, 4, 3);
+			this->Name = L"LoginForm";
+			this->Text = L"Login Form";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void btnCancelar_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Close();
+	}
+	private: System::Void btnOK_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ email = this->tbEmail->Text;
+		String^ password = this->tbPassword->Text;
+
+		if (email->Length == 0 || password->Length == 0) {
+			MessageBox::Show("Please enter email and password",
+				"Email or Password Empty", MessageBoxButtons::OK);
+			return;
+	    }
+
+		try
+		{
+			//String^ connstring = "";//12:20 Pner link de coneccion con la base de datos
+			//SqlCopnnection sqlConn(connString);
+			//sqlConn.Open();
+
+			//String^ sqlquery = "SELECT * FROM
+			// users WHERE email=@email AND password=@pwd;";
+			//command.Parameters->AddWithValue("@email", email);
+			//command.Parameters->AddWithValue("@pwd", password);
+
+			/*SqlDataReader^ reader = command.ExecuteReader();
+			if (reader->Read()) {
+				this->Close();
+			}
+			else {
+				MessageBox::Show("email or password is incorrect",
+					"Email or Password Error", MessageBoxButtons::OK);
+			}*/
+		}
+		catch (Exception^ e)
+		{
+			MessageBox::Show("Failed to connect to database",
+				"Database Connection Error",MessageBoxButtons::OK);
+		}
+	}
+};
+}
