5a435858b0f8529fe3b41089316dbc6fb8c39043
gonzalo alvines <gonzaloalvines@gmail.com>
2024-11-26 14:45:40 -0500

    agregando codigo

--- inmobiliariaVer1/BaseDatosInmobiliaria.h
+++ inmobiliariaVer1/BaseDatosInmobiliaria.h
@@ -1,4 +1,18 @@
+#pragma once
+using namespace std;
+using namespace System::Data;
+using namespace System;
+using namespace MySql::Data::MySqlCliente;
 
 ref class BaseDatosInmobiliaria
 {
+private:
+	String^ connectionString;
+	MySqlConnection^ conn;
+
+public:
+	BaseDatosInmobiliaria();
+	DataTable^ getData();
+	void abrirConexion();
+	void cerrarConexion();
 };
