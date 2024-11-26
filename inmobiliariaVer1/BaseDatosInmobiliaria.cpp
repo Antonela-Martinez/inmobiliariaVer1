1b653d8451a14e80d3bb1e0be402302ebdf59c4f
gonzalo alvines <gonzaloalvines@gmail.com>
2024-11-26 15:03:04 -0500

    nuevo cambio

--- inmobiliariaVer1/BaseDatosInmobiliaria.cpp
+++ inmobiliariaVer1/BaseDatosInmobiliaria.cpp
@@ -1,2 +1,28 @@
-
+#include "pch.h"
 #include "BaseDatosInmobiliaria.h"
+
+BaseDatosInmobiliaria::BaseDatosInmobiliaria() {
+	//metodo constructor para iniciar la base de datos
+	this->connectionString = "datasource=localhost; username=root; password=""; database=inmobiliaria; ";
+	this->conn = gcnew MySqlConnection(this->connectionString);
+}
+
+
+void BaseDatosInmobiliaria::abrirConexion() {
+	this->conn->Open();
+}
+
+void BaseDatosInmobiliaria::cerrarConexion() {
+	this->conn->Close();
+}
+
+DataTable^ BaseDatosInmobiliaria::getData() {
+
+	String^ sql = "select * from empleados";
+	MySqlCommand^ cursor = gcnew MySqlCommand(sql, this->conn);
+	MySqlDataAdapter^ data = gcnew MySqlDataAdapter(cursor);
+	DataTable^ tabla = gcnew DataTable();
+	data->Fill(tabla);//llenando datos a tabla
+	return tabla;
+}
+
